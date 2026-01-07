37a908b0af35498ac9374cc80bc429750b1cc683
Richard Liu <richardadesigner@gmail.com>
2026-01-07 00:04:08 -0800

    update

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 function CaesarCipher({ input, shift }) {
-
+    var originalOrderUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var originalOrderLow = "abcdefghijklmnopqrstuvwxyz";
+    var result = "";
+    var foundFlag = false;
+    for (var i = 0; i < input.length; i++) {
+        foundFlag = false;
+        for (var j = 0; j < originalOrderUp.length; j++) {
+            if (input[i] == originalOrderUp[j]) {
+                result = result + originalOrderUp[(j + shift) % 26];
+                foundFlag = true;
+            }
+        }
+        for (var k = 0; k < originalOrderLow.length; k++) {
+            if (input[i] == originalOrderLow[k]) {
+                result = result + originalOrderLow[(k + shift) % 26];
+                foundFlag = true;
+            }
+        }
+        if (foundFlag == false) {
+            result = result + input[i];
+        }
+    }
+    this.result = result;
 }
